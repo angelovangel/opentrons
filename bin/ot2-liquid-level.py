b8b97d8b6c67614d900cd567e276a8f294ec6a75
angelovangel <aangeloo@gmail.com>
2022-08-04 17:03:08 +0300

    new script monitor liquid level

--- bin/ot2-liquid-level.py
+++ bin/ot2-liquid-level.py
@@ -8,16 +8,25 @@ metadata = {
     'apiLevel': '2.8'
 }
 
+class VolumeTrack:
+        def __init__(self, type, well, volume, height):
+            self.type = type
+            self.well = well
+            self.volume = volume
+            self.height = height
+        
+        def update_vol(self, vol):
+            self.volume = self.volume - vol
+            print(f'Current volume is {self.volume} ul')
+
 def run(ctx: protocol_api.ProtocolContext):
 
     epitubes = ctx.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '5', 'Epi tubes')
     tips20_single = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['1']]
     s20 = ctx.load_instrument('p20_single_gen2', 'left', tip_racks=tips20_single)
 
-    class LevelTransfer:
-        def __init__(self, volume, height):
-            self.volume = volume
-            self.height = height
-        
-        def aspirate(self, vol):
-            s20.aspirate
+    def mytransfer(vol, src, dest):
+        if vol <= 0: return
+        s20.pick_up_tip()
+        s20.aspirate()
+

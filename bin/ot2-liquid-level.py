3b9fc96ea12f0f90713bd225ae23af411e475413
angelovangel <aangeloo@gmail.com>
2022-08-05 19:29:14 +0300

    track liquid level

--- bin/ot2-liquid-level.py
+++ bin/ot2-liquid-level.py
@@ -8,16 +8,15 @@ metadata = {
     'apiLevel': '2.8'
 }
 
-class VolumeTrack:
-        def __init__(self, type, well, volume, height):
-            self.type = type
-            self.well = well
-            self.volume = volume
-            self.height = height
+# class VolumeTrack:
+#         def __init__(self, type, location, volume):
+#             self.location = location
+#             self.volume = volume
+#             #self.height = height
         
-        def update_vol(self, vol):
-            self.volume = self.volume - vol
-            print(f'Current volume is {self.volume} ul')
+#         def update_vol(self, vol):
+#             self.volume = self.volume - vol
+#             print(f'Current volume is {self.volume} ul')
 
 def run(ctx: protocol_api.ProtocolContext):
 
@@ -25,8 +24,36 @@ def run(ctx: protocol_api.ProtocolContext):
     tips20_single = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['1']]
     s20 = ctx.load_instrument('p20_single_gen2', 'left', tip_racks=tips20_single)
 
-    def mytransfer(vol, src, dest):
-        if vol <= 0: return
+    def mytransfer(vol, srctype, srcwell, desttype, destwell):
+
         s20.pick_up_tip()
-        s20.aspirate()
+        s20.aspirate(vol, srctype.wells_by_name()[srcwell])
+        s20.dispense(vol, desttype.wells_by_name()[destwell])
+        s20.drop_tip()
+    
+    def track_volumes(labware, init_vol):
+        wells = dict(labware.wells_by_name())
+        for i in wells:
+            wells[i] = init_vol
+        return wells
+    
+    sourcewells=["A1", "C1", "A1"]
+    destwells=["A1","A3", "B2"]
+    volumes=[10.00, 6.00, 5.00]
+    
+    # initialise to track these
+    epiwells = track_volumes(epitubes, 100)
+        
+    for i, v in enumerate(sourcewells):
+        
+        mytransfer(volumes[i], epitubes, sourcewells[i], epitubes, destwells[i])
+        epiwells[v] = epiwells[v] - volumes[i]
+        
+    print(epiwells)
+
+    for i in range(10):
+        s20.transfer(i, epitubes['C1'], epitubes['D1'])
+        epiwells['C1'] = epiwells['C1'] - i
+        print(epiwells['C1'])
+
 

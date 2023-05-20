aab6b74b12e77af754236dd3445a7fa9aa2ce30d
angelovangel <aangeloo@gmail.com>
2023-05-20 08:10:06 +0300

    finish normalize protocol

--- protocols/06-normalize-dna.py
+++ protocols/06-normalize-dna.py
@@ -9,24 +9,25 @@ metadata = {
 	'apiLevel': '2.8'
 }
 
-sourcewells1=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
-destwells1=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
-volume1=[1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#sourcewells2= this is water, defined as constant below
-destwells2=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
+# destwells is fixed
+sourcewells=['A1', '', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
+destwells=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
+volume1=[1, 0, 10, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# water
 volume2=[9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 left_mount = 'p20_single_gen2'
 right_mount = 'p20_multi_gen2'
 watersource = 'A1'
-total_rxn_vol = 10
 
 # exit early if there is something wrong with the dest wells
-if len(destwells1) != 96:
+if len(destwells) != 96:
 	exit("Please make sure that there are 96 destination wells! Check that the template is correct...")
 
 ######################## BEGIN Calculations for full column transfer ########################
 # the requirement is that:
+# for ONE source column all rows go to ONE dest column AND 
+# there is row correspondence, i.e. A-A, B-B...H-H AND
 # the volumes are the same for the whole column
 scols1_fulltransfer = []
 dcols1_fulltransfer = []
@@ -34,17 +35,20 @@ svol1_fulltransfer = []
 
 for i in range(0, 95, 8):
 	svol1 = [vol for vol in volume1[i:i + 8]]
-	scols1 = [col[1:] for col in sourcewells1[i:i + 8]]
-	dcols1 = [col[1:] for col in destwells1[i:i + 8]]
+	scols1 = [col[1:] for col in sourcewells[i:i + 8]]
+	dcols1 = [col[1:] for col in destwells[i:i + 8]]
 	
-	if svol1.count(svol1[0]) == len(svol1): # all volumes in column are equal
+	if ([row[:1] for row in sourcewells[i:i + 8]] ==  [row[:1] for row in destwells[i:i + 8]] and # there is row correspondence 
+		scols1.count(scols1[0]) == len(scols1) and # all wells in the batch of 8 are the same column
+		svol1.count(svol1[0]) == len(svol1) # all volumes in column are equal
+		): 
 		# collect data for transfer
 		scols1_fulltransfer.append( scols1[0] )
 		dcols1_fulltransfer.append( dcols1[0] )
 		svol1_fulltransfer.append( svol1[0] )
 
 # set the vol1 for the whole col transfers to 0
-for i, v in enumerate(destwells1):
+for i, v in enumerate(destwells):
 	if v[1:] in dcols1_fulltransfer:
 		volume1[i] = 0
 		
@@ -56,12 +60,9 @@ def run(ctx: protocol_api.ProtocolContext):
 
 	destplate = ctx.load_labware('pcrplate_96_wellplate_200ul', '5', 'Destination plate') # stack of 96 well base plate and PCR plate
 	sourceplate = ctx.load_labware('pcrplate_96_wellplate_200ul', '4', 'Source plate') # stack of 96 well base plate and PCR plate
-	
 	sourcetube = ctx.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '7', 'Tube rack')
-
 	tips20_single = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['10', '11']]
 	tips20_multi = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['3']]
-
 	s20 = ctx.load_instrument(left_mount, mount='left', tip_racks=tips20_single)
 	m20 = ctx.load_instrument(right_mount, mount='right', tip_racks=tips20_multi)
 
@@ -71,7 +72,7 @@ def run(ctx: protocol_api.ProtocolContext):
 		s20.distribute(	
 			volume2,
 			sourcetube.wells_by_name()[watersource], 
-			[ destplate.wells_by_name()[i] for i in destwells2 ], 
+			[ destplate.wells_by_name()[i] for i in destwells ], 
 			new_tip = 'always', 
 			touch_tip = False
 		)
@@ -85,7 +86,7 @@ def run(ctx: protocol_api.ProtocolContext):
 			sourceplate.wells_by_name()['A' + scols1_fulltransfer[i]], 
 			destplate.wells_by_name()['A' + dcols1_fulltransfer[i]], 
 			new_tip = 'always', 
-			mix_after = (3, 4), 
+			mix_after = (3, svol1_fulltransfer[i]*0.7), 
 			blow_out = True, 
 			blowout_location = 'destination well' 
 			)
@@ -93,13 +94,15 @@ def run(ctx: protocol_api.ProtocolContext):
 	
 	# transfer what is left with s20
 	ctx.comment("Single channel transfer DNA ")
-	if any(vol > 0 for vol in volume1):
-		s20.transfer(
-			[v for v in volume1 if v > 0],
-			[ sourceplate.wells_by_name()[v] for i, v in enumerate(sourcewells1) if volume1[i] > 0],
-			[ destplate.wells_by_name()[v] for i, v in enumerate(destwells1) if volume1[i] > 0], 
+	
+	for i, v in enumerate(volume1):
+		if v > 0:
+			s20.transfer(
+			v,
+        	sourceplate.wells_by_name()[sourcewells[i]] ,
+        	destplate.wells_by_name()[destwells[i]],
 			new_tip = 'always',
-			mix_after = (3, 4), 
+			mix_after = (3, v * 0.7), 
 			blow_out = True, 
 			blowout_location = 'destination well'
 			)

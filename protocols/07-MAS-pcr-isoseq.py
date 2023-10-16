34bf54532c6d9e93ad7efc16cc48dcc24b57f68e
angelovangel <aangeloo@gmail.com>
2023-10-16 07:27:36 +0300

    working mas seq pcr logic

--- protocols/07-MAS-pcr-isoseq.py
+++ protocols/07-MAS-pcr-isoseq.py
@@ -10,7 +10,7 @@ metadata = {
     'apiLevel': '2.13'
 }
 
-nsamples = 2
+nsamples = 6
 ncycles = 9
 RM2well = 'D6'
 primerwells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4']
@@ -25,10 +25,10 @@ def run(ctx: protocol_api.ProtocolContext):
     startblock = ctx.load_labware('opentrons_24_aluminumblock_nest_0.5ml_screwcap', '5', 'Alu block')
     pcrplate = odtc.load_labware('biorad_96_wellplate_200ul_pcr') # IMPORTANT - use biorad plates!!!
 
-    tips20_single = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['1']]
-    tips20_multi = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['2', '3']]
+    tips20_single = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['1', '2']]
+    #tips20_multi = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['2', '3']]
     s20 = ctx.load_instrument('p20_single_gen2', mount='left', tip_racks=tips20_single)
-    m20 = ctx.load_instrument('p20_multi_gen2', mount='right', tip_racks=tips20_multi)
+    #m20 = ctx.load_instrument('p20_multi_gen2', mount='right', tip_racks=tips20_multi)
 
     # set s20 flow rates globally, default is 7.56 
     s20.flow_rate.aspirate = 5
@@ -51,10 +51,12 @@ def run(ctx: protocol_api.ProtocolContext):
         s20.transfer(
             2.5,
             [startblock.wells_by_name()[well] for well in primerwells[:8]],
-            pcrplate.columns()[i]
+            pcrplate.columns()[i*2], 
+            new_tip = 'always'
         )
         s20.transfer(
             2.5,
             [startblock.wells_by_name()[well] for well in primerwells[8:]],
-            pcrplate.columns()[i + 1]
+            pcrplate.columns()[i*2 + 1],
+            new_tip = 'always'
         )

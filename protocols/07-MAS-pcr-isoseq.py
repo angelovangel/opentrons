56ba738f32c9453184c10d2b83634f067b78f690
angelovangel <aangeloo@gmail.com>
2023-10-16 16:18:07 +0300

    better labware placing

--- protocols/07-MAS-pcr-isoseq.py
+++ protocols/07-MAS-pcr-isoseq.py
@@ -1,6 +1,8 @@
 # MAS seq Iso-Seq PacBio
 # doing only the MAS PCR step
-# prepare 1 RM2 for each sample and place in 'A5'..'B6' - max 6 samples
+# prepare 1 RM2 for each sample and place in A1..B2 of rack - max 6 samples
+# empty 1.5 ml tubes in A5..B6 of rack to collect pools after PCR 
+# the 16 primer mixes go in A1..D4 of 2 ml aluminium block
 
 from opentrons import protocol_api
 
@@ -14,9 +16,10 @@ metadata = {
 ncycles = 1
 primervol = 2
 RM2vol = 18
-RM2wells = ['A5', 'B5', 'C5', 'D5', 'A6', 'B6']
+RM2wells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2'] # on rack
+poolwells = ['A5', 'B5', 'C5', 'D5', 'A6', 'B6'] # on rack
 nsamples = len(RM2wells)
-primerwells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4']
+primerwells = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4'] # on block
 
 pcrprofile = [
     {'temperature':98, 'hold_time_seconds':20},
@@ -35,8 +38,8 @@ def run(ctx: protocol_api.ProtocolContext):
     ctx.comment('--------------------------------')
     
     odtc = ctx.load_module(module_name='thermocyclerModuleV2')
-    startblock = ctx.load_labware('opentrons_24_aluminumblock_nest_0.5ml_screwcap', '5', 'Alu block')
-    rack = ctx.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '4', 'Tube rack')
+    startblock = ctx.load_labware('opentrons_24_aluminumblock_nest_0.5ml_screwcap', '4', 'Alu block')
+    rack = ctx.load_labware('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', '5', 'Tube rack')
     pcrplate = odtc.load_labware('biorad_96_wellplate_200ul_pcr') # IMPORTANT - use biorad plates!!!
 
     tips20_single = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot) for slot in ['1', '2']]
@@ -45,8 +48,8 @@ def run(ctx: protocol_api.ProtocolContext):
     #m20 = ctx.load_instrument('p20_multi_gen2', mount='right', tip_racks=tips20_multi)
 
     # set s20 flow rates globally, default is 7.56 
-    s20.flow_rate.aspirate = 5
-    s20.flow_rate.dispense = 4
+    # s20.flow_rate.aspirate = 5
+    # s20.flow_rate.dispense = 4
 
     # setup ODTC
     odtc.open_lid()
@@ -59,11 +62,13 @@ def run(ctx: protocol_api.ProtocolContext):
         ctx.comment("--------------------------------------")
         s20.distribute(
             RM2vol,
-            startblock.wells_by_name()[v],
+            rack.wells_by_name()[v],
             [pcrplate.columns()[col + i*2] for col in range(2)]
         )
-    # 
+    # Primer mix addition
     for i in range(nsamples):
+        ctx.comment("Adding primermix to pcrplate")
+        ctx.comment("--------------------------------------")
         s20.transfer(
             primervol,
             [startblock.wells_by_name()[well] for well in primerwells[:8]],
@@ -89,13 +94,13 @@ def run(ctx: protocol_api.ProtocolContext):
     ctx.comment("--------------------------------------")
 
     # Consolidate PCRs
-    for i, v in enumerate(RM2wells):
-        ctx.comment("Consolidating sample " + str(RM2wells[i]) + ' into ' + str(rack.wells()[i]))
+    for i, v in enumerate(poolwells):
+        ctx.comment("Consolidating sample " + str(RM2wells[i]) + ' into ' + v)
         ctx.comment("--------------------------------------")
         s20.consolidate(
             RM2vol+primervol, 
             [pcrplate.columns()[col + i*2] for col in range(2)], 
-            rack.wells()[i]
+            rack.wells_by_name()[v]
         )
         ctx.comment("--------------------------------------")
 

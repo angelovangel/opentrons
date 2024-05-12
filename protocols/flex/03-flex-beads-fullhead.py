2b98252ef9fdc58aae76a3821646f882352b2802
angelovangel <aangeloo@gmail.com>
2024-05-12 10:41:22 +0300

    distribute etoh in flex beads cleanup

--- protocols/flex/03-flex-beads-fullhead.py
+++ protocols/flex/03-flex-beads-fullhead.py
@@ -4,8 +4,8 @@ from opentrons import types
 import math
 
 metadata = {
-    "protocolName": "Beads cleanup Flex 96-channel, partial loading",
-    "description": """This protocol is for performing bead cleanup with variable number of columns (1 to 6)""",
+    "protocolName": "Beads cleanup Flex 96-channel, full head loading",
+    "description": """This protocol is for performing bead cleanup with variable number of columns (1 to 12)""",
     "author": "angel.angelov@kaust.edu.sa"
     }
 
@@ -15,24 +15,22 @@ requirements = {
     }
 
 ###     Variables            ###
-ncols =       2
-# tip cols needed  = 2 + ncols*7
-#n_tipboxes = math.ceil((2 + ncols*7)/12)
+ncols =       7
 samplevol =  50
 beadspos =  'A1'
 beadsvol =   50
 ebpos =     'A2'
 ebvol =      40
 etohvol =   150
-inctime =     10
+etohpos =   ['A3', 'A4']
+inctime =    10
 speed_factor_aspirate = 1
-#speed_factor_dispence = 1
 DRY_RUN = False
 BEADSMIX = False
 ################################
 
-if ncols < 1 | ncols > 7:
-    quit("ncols must be between 1 and 6")
+# if ncols < 1 | ncols > 7:
+#     quit("ncols must be between 1 and 6")
 
 def comment(myctx, message):
     myctx.comment("-----------")
@@ -41,7 +39,7 @@ def comment(myctx, message):
 
 
 def run(ctx: protocol_api.ProtocolContext):
-    rack_partial = ctx.load_labware(load_name="opentrons_flex_96_filtertiprack_200ul", location='A1')
+    rack_partial = ctx.load_labware(load_name="opentrons_flex_96_filtertiprack_1000ul", location='A1')
     fullpositions = ['B3', 'C3', 'D3', 'A2', 'B2']
     rack_full_1, rack_full_2, rack_full_3, rack_full_4, rack_full_5 = [
         ctx.load_labware(
@@ -52,6 +50,7 @@ def run(ctx: protocol_api.ProtocolContext):
     
     pip = ctx.load_instrument("flex_96channel_1000")
     original_flow_rate_aspirate = pip.flow_rate.aspirate
+    original_flow_rate_dispense = pip.flow_rate.dispense
     
     # ----------------------------------------------
     # modules
@@ -59,13 +58,23 @@ def run(ctx: protocol_api.ProtocolContext):
     # ----------------------------------------------
     # labware
     reservoir = ctx.load_labware("nest_12_reservoir_15ml", "D2")
-    etoh = ctx.load_labware("axygen_1_reservoir_90ml", "C2")
+    #etoh = ctx.load_labware("axygen_1_reservoir_90ml", "C2")
     plate1 = ctx.load_labware("biorad_96_wellplate_200ul_pcr", "D1")
     plate2 = ctx.load_labware("biorad_96_wellplate_200ul_pcr", "B1")
     trash = ctx.load_trash_bin("A3")
 
     #-----------------------------------------------
     # helper functions
+    def pip_config(type):
+        if type == 'partial':
+            pip.configure_nozzle_layout(
+            style=COLUMN,
+            start="A12",
+            tip_racks=[rack_partial]
+            )
+        elif type == 'full':
+            pip.configure_nozzle_layout(style = ALL)
+
     def slow_tip_withdrawal(pipette, well):
         factor_slow = 40
         pipette.default_speed /= factor_slow
@@ -101,11 +110,7 @@ def run(ctx: protocol_api.ProtocolContext):
 
 
     ########################################################################
-    pip.configure_nozzle_layout(
-        style=COLUMN,
-        start="A12",
-        tip_racks=[rack_partial]
-    )
+    pip_config('partial')
     ########################################################################
 
     pip.flow_rate.aspirate = original_flow_rate_aspirate / speed_factor_aspirate
@@ -132,7 +137,7 @@ def run(ctx: protocol_api.ProtocolContext):
     
     # Mixing beads during inc
     # full head loading ################################################################
-    pip.configure_nozzle_layout(style = ALL)
+    pip_config('full')
     # full head loading ################################################################
 
     if BEADSMIX:
@@ -170,28 +175,38 @@ def run(ctx: protocol_api.ProtocolContext):
     remove(samplevol + beadsvol, plate1['A1'], trash, type = 'full')
     pip.drop_tip()
 
-    # EtOH washes - racks 2 and 3
+    # EtOH washes - distribute from reservoir, use rack2 for etoh removal
     comment(ctx, "EtOH washes")
-    for i in [rack_full_2, rack_full_3]:
-        pip.pick_up_tip(i['A1'])
-        pip.aspirate(etohvol, etoh['A1'], rate=0.7)
-        pip.aspirate(10, etoh['A1'].top().move(types.Point(0,0,1)))
-        pip.dispense(etohvol + 10, plate1['A1'].top().move(types.Point(0,0,-1)), rate=0.2)
-        pip.aspirate(10, plate1['A1'].top().move(types.Point(0,0,1))) # air gap
+    # determine which reservoir pos to aspirate from
+    for i in range(2):
+        i += 1
+        ########################################################################
+        pip_config('partial')
+        ########################################################################
+        pip.flow_rate.dispense = original_flow_rate_dispense * 0.2
+        if ncols <= 6:
+            pip.distribute(etohvol, reservoir[etohpos[0]], [i.top().move(types.Point(0,0,1)) for i in rowA_start[0:ncols]])
+        else:
+            pip.distribute(etohvol, reservoir[etohpos[0]], [i.top().move(types.Point(0,0,1)) for i in rowA_start[0:6]])
+            pip.distribute(etohvol, reservoir[etohpos[1]], [i.top().move(types.Point(0,0,1)) for i in rowA_start[6:ncols]])
         if not DRY_RUN:
             ctx.delay(seconds=25)
+        pip.flow_rate.dispense = original_flow_rate_dispense
+
+        pip_config('full')
+        pip.pick_up_tip(rack_full_2['A1'])
         remove(etohvol, plate1['A1'], trash, type = 'full')
-        pip.drop_tip()
+        if i < 2:
+            pip.return_tip()
+        else:
+            pip.drop_tip()
+
 
     # Move plate back to resuspend beads - column loading
     ctx.move_labware(plate1, 'D1', use_gripper=True)
 
     ########################################################################
-    pip.configure_nozzle_layout(
-        style=COLUMN,
-        start="A12",
-        tip_racks=[rack_partial]
-    )
+    pip_config('partial')
     ########################################################################
     
     comment(ctx, "Resuspend beads")
@@ -206,7 +221,7 @@ def run(ctx: protocol_api.ProtocolContext):
     )
 
     # full head loading ################################################################
-    pip.configure_nozzle_layout(style = ALL)
+    pip_config('full')
     # full head loading ################################################################
 
     comment(ctx, 'Incubate ' + str(inctime) + ' minutes')

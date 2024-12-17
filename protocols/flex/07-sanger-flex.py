fe91f0fe31bd537696b6dd9053887d09978cad08
angelovangel <aangeloo@gmail.com>
2024-12-17 08:42:29 +0300

    add flex sanger

--- protocols/flex/07-sanger-flex.py
+++ protocols/flex/07-sanger-flex.py
@@ -19,22 +19,31 @@ def comment(myctx, message):
 ### Parameters ###
 def add_parameters(parameters):
     parameters.add_int(
-        variable_name="rxn_vol",
+        variable_name="sample_vol",
         display_name="Sample volume",
-        description="Sanger sample volume",
+        description="Sanger sample volume (ul)",
         default=15,
-        minimum=10,
+        minimum=5,
         maximum=15,
+        unit="uL"
+    )
+    parameters.add_int(
+        variable_name="mm_vol",
+        display_name="Mastermix volume",
+        description="BigDye mastermix volume (ul)",
+        default=10,
+        minimum=5,
+        maximum=10,
         unit="uL"
     )
 
     parameters.add_str(
         variable_name="source_type",
         display_name="Source type",
-        description="Start reaction labware type",
+        description="Source plate/strip labware type",
         choices=[
             {"display_name": "Biorad plate", "value": "biorad_96_wellplate_200ul_pcr"},
-            {"display_name": "Stack of strips in Biorad plate", "value": "stack_plate_biorad96well"},
+            {"display_name": "Stack in Biorad plate", "value": "stack_plate_biorad96well"},
         ],
         default="stack_plate_biorad96well",
     )
@@ -43,7 +52,7 @@ def add_parameters(parameters):
         variable_name='ncolumns', 
         display_name="Number of columns to process",
         description="Number of columns to process",
-        default=12,
+        default=10,
         minimum=1,
         maximum=12,
         unit="columns"
@@ -53,7 +62,8 @@ def add_parameters(parameters):
 
 def run(ctx: protocol_api.ProtocolContext):
     
-    samplevol = ctx.params.rxn_vol
+    samplevol = ctx.params.sample_vol
+    mmvol = ctx.params.mm_vol
     ncols = ctx.params.ncolumns
 
     # tips and pipette
@@ -65,17 +75,14 @@ def run(ctx: protocol_api.ProtocolContext):
             ) for loc in full_positions
     ]
 
-    partial1000 = ctx.load_labware(load_name="opentrons_flex_96_filtertiprack_1000ul", location='C4')
-    partial50 = ctx.load_labware(load_name="opentrons_flex_96_filtertiprack_50ul", location='B4')
+    partial200 = ctx.load_labware(load_name="opentrons_flex_96_filtertiprack_200ul", location='B2')
+    
     pip = ctx.load_instrument("flex_96channel_1000")
     
     #labware
-    start_stack = ctx.load_labware("stack_plate_biorad96well", "C2")
-    start_plate = ctx.load_labware("stack_plate_biorad96well", "C1")
+    start_plate = ctx.load_labware(ctx.params.source_type, "C2")
     res = ctx.load_labware("nest_12_reservoir_15ml", "D2")
-
-    rxn_stack = ctx.load_labware("stack_plate_biorad96well", "D1")
-    
+    rxn_stack = ctx.load_labware("stack_plate_biorad96well", "C1")
     trash = ctx.load_waste_chute()
     mm_pos = 'A1'
 
@@ -95,100 +102,25 @@ def run(ctx: protocol_api.ProtocolContext):
     
     # distribute MM to rxn_stack
     ########################################################################
-    pip_config('partial', partial1000)
+    pip_config('partial', partial200)
     ########################################################################
-    ctx.move_labware(partial1000, 'A3', use_gripper=True)
+    
     pip.well_bottom_clearance.dispense = 1
-
-    pip.distribute(10 ,res[mm_pos], rxn_stack.columns()[:ncols])
-
-    # if ctx.params.prep_qbit:
-    #     # distribute qbit rgnt
-    #     ########################################################################
-    #     pip_config('partial', partial1000)
-    #     ########################################################################
+    pip.distribute(10 ,res[mm_pos], rxn_stack.rows()[0][:ncols])
     
-    #     if ncols2 > 0:
-    #         qbit_cols = qbit_stack1.columns()[::2][:ncols1] + qbit_stack2.columns()[::2][:ncols2]
-    #     else:
-    #         qbit_cols = qbit_stack1.columns()[::2][:ncols1]
-    #     comment(ctx, "Adding qbit reagent to " + str(ncols1 + ncols2) + " columns")
+
+    # transfer reactions
+    ########################################################################
+    pip_config('full', rack_full_1)
+    ########################################################################
+    pip.well_bottom_clearance.aspirate = 0
+    pip.well_bottom_clearance.dispense = 3
+    pip.flow_rate.aspirate = 30
     
-    #     ctx.move_labware(partial1000, 'A3', use_gripper=True)
-    #     # 15 ml are enough for 9 columns
-        
-    #     pip.well_bottom_clearance.dispense = 15
-    #     pip.distribute(
-    #         200 - samplevol, 
-    #         res[qbit_pos], 
-    #         qbit_cols, 
-    #         touch_tip=False
-    #     )
-    #     pip.well_bottom_clearance.dispense = 1
-    #     ctx.move_labware(partial1000, 'C4', use_gripper=True)
-
-    #     ########################################################################
-    #     pip_config('full', rack_full_1)
-    #     ########################################################################
-
-    #     pip.pick_up_tip(rack_full_1['A1'])
+    pip.transfer(
+        samplevol, 
+        start_plate['A1'], 
+        rxn_stack['A1'], 
+        mix_after = (6, 10)
+    )
     
-    #     pip.aspirate(18, waterlid['A1'],rate = 0.1)
-    #     pip.air_gap(3)
-    #     pip.aspirate(2, start_stack1['A1'], rate = 0.1)
-    #     pip.dispense(location = dil_plate['A1'])
-    #     pip.well_bottom_clearance.aspirate = 3
-    #     pip.well_bottom_clearance.dispense = 3
-    #     pip.mix(10, 15)
-    #     pip.well_bottom_clearance.aspirate = 1
-    #     pip.well_bottom_clearance.dispense = 1
-
-    #     pip.aspirate(samplevol, dil_plate['A1'], rate = 0.1)
-    #     pip.air_gap(3)
-    #     pip.dispense(location = qbit_stack1['A1'])
-    #     pip.well_bottom_clearance.aspirate = 3
-    #     pip.well_bottom_clearance.dispense = 3
-    #     pip.mix(10, 50)
-    #     pip.well_bottom_clearance.aspirate = 1
-    #     pip.well_bottom_clearance.dispense = 1
-    #     pip.drop_tip()
-
-    #     if ncols2 > 0:
-    #         pip.pick_up_tip(rack_full_2['A1'])
-    
-    #         pip.aspirate(18, waterlid['A1'],rate = 0.1)
-    #         pip.air_gap(3)
-    #         pip.aspirate(2, start_stack2['A1'], rate = 0.1)
-    #         pip.dispense(location = dil_plate['A2'])
-    #         pip.well_bottom_clearance.aspirate = 3
-    #         pip.well_bottom_clearance.dispense = 3
-    #         pip.mix(10, 15)
-    #         pip.well_bottom_clearance.aspirate = 1
-    #         pip.well_bottom_clearance.dispense = 1
-    
-    #         pip.aspirate(samplevol, dil_plate['A2'], rate = 0.1)
-    #         pip.air_gap(3)
-    #         pip.dispense(location = qbit_stack2['A1'])
-    #         pip.well_bottom_clearance.aspirate = 3
-    #         pip.well_bottom_clearance.dispense = 3
-    #         pip.mix(10, 50)
-    #         pip.well_bottom_clearance.aspirate = 1
-    #         pip.well_bottom_clearance.dispense = 1
-    #         pip.drop_tip()
-
-    # if ctx.params.prep_plate:
-    #     ########################################################################
-    #     pip_config('partial', partial50)
-    #     ########################################################################
-        
-    #     ctx.move_labware(partial50, 'A3', use_gripper=True)
-
-    #     if ncols2 > 0:
-    #         final_cols = start_stack1.columns()[::2][:ncols1] + start_stack2.columns()[::2][:ncols2]
-    #     else:
-    #         final_cols = start_stack1.columns()[::2][:ncols1]
-        
-    #     pip.flow_rate.aspirate = pip.flow_rate.aspirate / 4
-    #     pip.flow_rate.dispense = pip.flow_rate.dispense / 4
-    #     pip.transfer(25, final_cols, final_plate.columns()[:ncols1 + ncols2], new_tip = 'always')
-    #     ctx.move_labware(partial50, 'B4', use_gripper = True)

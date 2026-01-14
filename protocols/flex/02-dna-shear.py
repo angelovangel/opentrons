c5781950dd083e60b4a5be6b593fe3fdba01d5ed
angelovangel <aangeloo@gmail.com>
2026-01-14 15:49:56 +0300

    shear fix

--- protocols/flex/02-dna-shear.py
+++ protocols/flex/02-dna-shear.py
@@ -1,3 +1,4 @@
+import math
 from opentrons import protocol_api
 from opentrons import types
 from opentrons.protocol_api import COLUMN, ALL
@@ -10,20 +11,17 @@ metadata = {
 
 requirements = {
     "robotType": "Flex",
-    "apiLevel": "2.16"
+    "apiLevel": "2.18"
     }
     
 ###     Variables            ###
-ncols = 2
+samples = 2
 tips = "opentrons_flex_96_filtertiprack_1000ul"
-mixvol = 290
-reps = 3
-speed_aspirate = 716
-speed_dispence = 716
-single_col_load = False
+reps = 8
+single_col_load = True
 ################################
 
-cols = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12']
+cols = math.ceil(samples/8)
 
 def run(ctx: protocol_api.ProtocolContext):
     ctx.load_trash_bin("A3")
@@ -32,7 +30,7 @@ def run(ctx: protocol_api.ProtocolContext):
     else:
         rack1000 = ctx.load_labware(load_name=tips, location="B3", adapter='opentrons_flex_96_tiprack_adapter')
         
-    sampleplate = ctx.load_labware('nest_96_wellplate_2ml_deep', 'B1')
+    samples = ctx.load_labware('nest_96_wellplate_2ml_deep', 'B1')
     pip = ctx.load_instrument("flex_96channel_1000", mount='left', tip_racks=[rack1000])
     
     if single_col_load:
@@ -41,24 +39,13 @@ def run(ctx: protocol_api.ProtocolContext):
             start="A12",
             tip_racks=[rack1000]
         )
-    pip.flow_rate.aspirate = speed_aspirate
-    pip.flow_rate.dispense = speed_dispence
+    
     
 
-
-    # currently only one column loading is supported by the API, but this will change
-    # for i in cols[:ncols]:
-    #     mymix(reps)
-    pip.pick_up_tip()
-    pip.mix(reps, mixvol, sampleplate['A1'])
-    pip.drop_tip()
-
-    mydict = {1: ['A1', 'A5', 'A9'], 2: ['A2', 'A6', 'A10'], 3: ['A3', 'A7', 'A11'], 4: ['A4', 'A8', 'A12']}
-    if single_col_load:
-        for _, (k, v) in enumerate( mydict.items() ):
-            print(k, v)
-            for well in v:
-                pip.pick_up_tip()
-                pip.mix(repetitions=k, volume=mixvol, location=sampleplate[well])
-                pip.drop_tip()
-                #mymix(mixvol, well, k, return_tip=False)
+    pip.flow_rate.aspirate=1000
+    pip.flow_rate.dispense=1000
+    for x in range(cols):
+        pip.pick_up_tip()
+        for i in range(reps):
+            pip.mix(100, 200, samples[x])
+        pip.drop_tip()

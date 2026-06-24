ba2ded00e472793d65dc65a875eb763d34eec15e
angelovangel <aangeloo@gmail.com>
2026-06-24 12:40:29 +0300

    params qbit flex

--- protocols/flex/10-qbit-assay-plate.py
+++ protocols/flex/10-qbit-assay-plate.py
@@ -27,6 +27,16 @@ def add_parameters(parameters):
 		maximum=12,
 		unit="columns"
 	)
+	parameters.add_int(
+		variable_name='sample_volume',
+		display_name="Sample volume",
+		description="Volume of sample to be transferred to final plate (ul)",
+		default=20,
+		minimum=1,
+		maximum=100,
+		unit="ul"
+	)
+
 	parameters.add_bool(
 		variable_name = 'prep_plate',
 		display_name = 'Prepare run plate',
@@ -50,9 +60,27 @@ def add_parameters(parameters):
 		],
 		default="nest_12_reservoir_15ml",
 	)
+	parameters.add_int(
+		variable_name='asp_offset',
+		display_name="Aspiration offset",
+		description="Offset from bottom of well for aspiration (mm)",
+		default=1,
+		minimum=0,
+		maximum=10,
+	)
+	parameters.add_int(
+		variable_name='disp_offset',
+		display_name="Dispense offset",
+		description="Offset from bottom of well for dispense (mm)",
+		default=1,
+		minimum=0,
+		maximum=20
+	)
 
 def run(ctx: protocol_api.ProtocolContext):
 	ncols = ctx.params.num_columns
+	asp = ctx.params.asp_offset
+	disp = ctx.params.disp_offset
 
 	tips50 = [ctx.load_labware("opentrons_flex_96_filtertiprack_50ul", loc) for loc in ['B3', 'B2']]
 	tips1000 = ctx.load_labware("opentrons_flex_96_filtertiprack_1000ul", "A3")
@@ -72,34 +100,34 @@ def run(ctx: protocol_api.ProtocolContext):
 	# distribute qbit reagent
 	pip_right.flow_rate.aspirate = 50
 	pip_right.flow_rate.dispense = 150
-	pip_right.well_bottom_clearance.dispense = 15
+
 	pip_right.distribute(
 		195, 
-		res['A1'],
-		qbit_plate.columns()[:ncols],
+		res['A1'].bottom(asp),
+		[col[0].bottom(disp) for col in qbit_plate.columns()[:ncols]]
 	)
-	pip_right.well_bottom_clearance.dispense = 1
+	
 
 	# dilute and add samples to qbit plate
 	pip_right.configure_nozzle_layout(style = ALL, tip_racks = tips50)
 	if ctx.params.prep_qbit:
 		for col in range(ncols):
 			pip_right.pick_up_tip()
-			pip_right.aspirate(8, waterlid['A1'], rate=0.1)
-			pip_right.aspirate(2, start_stack.rows()[0][col], rate=0.1)
-			pip_right.dispense(10, dil_plate.rows()[0][col])
-			pip_right.mix(repetitions = 5, volume = 18, location = dil_plate.rows()[0][col])
-			pip_right.aspirate(5, dil_plate.rows()[0][col], rate=0.1)
-			pip_right.dispense(5, qbit_plate.rows()[0][col])
-			pip_right.mix(repetitions = 5, volume = 40, location = qbit_plate.rows()[0][col])
+			pip_right.aspirate(8, waterlid['A1'].bottom(asp), rate=0.1)
+			pip_right.aspirate(2, start_stack.rows()[0][col].bottom(asp), rate=0.1)
+			pip_right.dispense(10, dil_plate.rows()[0][col].bottom(disp))
+			pip_right.mix(repetitions = 5, volume = 18, location = dil_plate.rows()[0][col].bottom(disp))
+			pip_right.aspirate(5, dil_plate.rows()[0][col].bottom(asp), rate=0.1)
+			pip_right.dispense(5, qbit_plate.rows()[0][col].bottom(disp))
+			pip_right.mix(repetitions = 5, volume = 40, location = qbit_plate.rows()[0][col].bottom(disp))
 			pip_right.drop_tip()
 	
 	if ctx.params.prep_plate:
 		pip_right.flow_rate.aspirate = 5
 		pip_right.flow_rate.dispense = 25
 		pip_right.transfer(
-			20, 
-			start_stack.rows()[0][:ncols], 
-			final_plate.rows()[0][:ncols], 
+			ctx.params.sample_volume, 
+			[well.bottom(asp) for well in start_stack.rows()[0][:ncols]], 
+			[well.bottom(disp) for well in final_plate.rows()[0][:ncols]], 
 			new_tip='always'
 		)
